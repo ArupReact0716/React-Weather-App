05fe8d050950e41ab1b97b49745878798847d600
Arup Maity <117928445+arup-maity@users.noreply.github.com>
2024-09-20 11:41:22 +0530

    Change the api.openweathermap.org api key

--- src/redux/reducer/WeatherSlice.js
+++ src/redux/reducer/WeatherSlice.js
@@ -5,7 +5,7 @@ export const CurrentWeather = createAsyncThunk(
    "weather/CurrentWeather",
    async (city, { rejectWithValue }) => {
       try {
-         const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&units=metric&cnt=7&appid=d94bcd435b62a031771c35633f9f310a`)
+         const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&units=metric&cnt=7&appid=e8bcb8ed34c76adf0610265917374452`)
          return response.data
       } catch (error) {
          return rejectWithValue(error.response.data);
